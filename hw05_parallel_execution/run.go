5f0cd5a3ad8392035ea06935593834013d9b35ff
User <user@local>
2024-12-09 01:00:23 +0300

    beta fixes

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -2,34 +2,35 @@ package hw05parallelexecution
 
 import (
 	"errors"
-	"fmt"
 	"sync"
 	"sync/atomic"
 )
 
-var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
-var suscessCount atomic.Int32
-var errorsCount atomic.Int32
+var (
+	ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
+	suscessCount           atomic.Int32
+	errorsCount            atomic.Int32
+)
 
 type Task func() error
 
 func Worker(wg *sync.WaitGroup, workerNum int, tasks <-chan Task, m int) {
 	defer wg.Done()
-	fmt.Println("STARTED Worker #", workerNum)
+	//fmt.Println("STARTED Worker #", workerNum)
 
 	for task := range tasks {
 		if int(errorsCount.Load()) >= m {
 			break
 		}
-		error := task()
-		if error != nil {
-			fmt.Printf("Worker #%d: got error\n", workerNum)
+		taskResult := task()
+		if taskResult != nil {
+			//fmt.Printf("Worker #%d: got error\n", workerNum)
 			errorsCount.Add(1)
 		} else {
-			fmt.Printf("Worker #%d: reprot for compleated task, sum: %d\n", workerNum, suscessCount.Add(1))
+			//fmt.Printf("Worker #%d: reprot for compleated task, sum: %d\n", workerNum, suscessCount.Add(1))
 		}
 	}
-	fmt.Println("Worker #", workerNum, " terminated")
+	//fmt.Println("Worker #", workerNum, " terminated")
 }
 
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
@@ -38,11 +39,14 @@ func Run(tasks []Task, n, m int) error {
 	var wg sync.WaitGroup
 	wg.Add(n)
 
+	suscessCount.Store(0)
+	errorsCount.Store(0)
+
 	for workerNum := range n {
 		workerNum++
 		go Worker(&wg, workerNum, jobs, m)
 	}
-	fmt.Println("Processing jobs")
+	//fmt.Println("Processing jobs")
 
 	for taskNum := range len(tasks) {
 		if tasks[taskNum] == nil {
@@ -50,12 +54,15 @@ func Run(tasks []Task, n, m int) error {
 		}
 
 		if int(errorsCount.Load()) >= m {
-			break
+			close(jobs)
+			wg.Wait()
+			//fmt.Printf("ER Finished with: Success: %d Errors: %d\n", suscessCount.Load(), errorsCount.Load())
+			return ErrErrorsLimitExceeded
 		}
 		jobs <- tasks[taskNum]
 	}
 	close(jobs)
 	wg.Wait()
-	fmt.Printf("Finished with:\n Success: %d\n Errors: %d", suscessCount.Load(), errorsCount.Load())
+	//fmt.Printf("Finished with: Success: %d Errors: %d\n", suscessCount.Load(), errorsCount.Load())
 	return nil
 }
